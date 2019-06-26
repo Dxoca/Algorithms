da6c119006dd4ca8f583f1a095e39ceec5083437
e <987284242@qq.com>
2019-06-26 20:30:18 +0800

    Annotation(注释)

--- src/CometOJ/Contest5_A.java
+++ src/CometOJ/Contest5_A.java
@@ -2,10 +2,10 @@ package CometOJ;
 import java.util.Scanner;
 
 /**
+ * @name 迫真字符串
  * @Test https://cometoj.com/contest/46/problem/A?problem_id=2021
  * @Author Dxoca
  */
-
 public class Contest5_A {
     public static void main(String[] args){
         Scanner cin =new Scanner(System.in);
@@ -17,15 +17,14 @@ public class Contest5_A {
             b[a[i]-'0']++;
         }
         boolean flag=true;
-        while (flag){
-            if(b[1]>=3&&b[4]>=2&&b[5]>=1){//重新排序后114514出现的最大次数
-                b[1]-=3;
-                b[4]-=2;
-                b[5]-=1;
+        while (flag) {
+            if (b[1] >= 3 && b[4] >= 2 && b[5] >= 1) {//重新排序后114514出现的最大次数
+                b[1] -= 3;
+                b[4] -= 2;
+                b[5] -= 1;
                 ans++;
-            }
-            else
-                flag=!flag;
+            } else
+                flag = !flag;
         }
         System.out.println(ans);
     }
