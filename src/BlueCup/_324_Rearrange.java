7dd771d94200b2bd6df8ead52ee0b1247b975ef6
e <987284242@qq.com>
2019-06-28 19:53:04 +0800

    优化

--- src/BlueCup/_324_Rearrange.java
+++ src/BlueCup/_324_Rearrange.java
@@ -4,7 +4,7 @@ package BlueCup;
  * @Author Dxoca
  * @Source Case08_Rearrange.java
  * 题8 需要排序的子数组
- * 给定一个无需数组arr 求出需要排序的最短子数组长度
+ * 给定一个无需数组arr 求出需要排序的最短子#数组长度#
  * 要求：O(n)
  * input arr 2 3 7 5 4 6  return 4 because only 7546 need Rearrange.
  */
@@ -17,29 +17,35 @@ public class _324_Rearrange {
         for (int i = 0; i < n; i++) {
 
             if (A[i] > max) {
-                max = A[i];
+                max = A[i];//最高峰
             }
             if (A[i] < max) {//只要低于 最高峰 右边拓展
                 r = i;
             }
-
-        }
-        for (int i = n - 1; i >= 0; i--) {
             if (A[i] < min) {//最低的点
                 min = A[i];
             }
             if (A[i] > min) {
                 l = i;
             }
+
         }
+        for (int i = n - 1; i >= 0; i--) {//倒查
+            if (A[i] < min) {//最低的点
+                min = A[i];
+            }
+            if (A[i] > min) {
+                l = i;
+            }
+        }//>>O(2n)
         if (l == -1) {
             return new int[]{0, 0};
         }
         return new int[] {l,r};
     }
     public static void main(String[] args) {
-        int[] A ={1, 4, 6, 5, 9, 10};
+        int[] A ={1, 4, 6, -1, 9, 6};
         int[] res=_324_Rearrange.findSegment(A,A.length);
-        System.out.println(A[res[0]]+","+A[res[1]]);
+        System.out.println(A[res[0]]+","+A[res[1]]+":"+(res[1]-res[0]+1));
     }
 }
