59dea84b6905a7aca660e3a9b81b97f28e153fec
e <987284242@qq.com>
2019-06-28 19:53:22 +0800

    败北

---
+++ src/CometOJ/Contest6_A.java
@@ -0,0 +1,23 @@
+package CometOJ;
+
+import java.util.Scanner;
+
+public class Contest6_A {
+    public static void main(String[] args){
+        Scanner cin =new Scanner(System.in);
+        int n=cin.nextInt() ,m=cin.nextInt(),x,y;
+        boolean who =false;//Ill
+        for (int i = 0; i < m; i++) {
+            x=cin.nextInt();
+            y=cin.nextInt();
+            //失败！！ 不会 图论（离散数学）
+
+        }
+        if(who){
+            System.out.println("Illyasviel");
+        }
+        else
+            System.out.println("Star-dust");
+
+    }
+}
