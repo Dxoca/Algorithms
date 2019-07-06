2129a98f9deed66825cbfda125aa85bfc9bd0374
e <987284242@qq.com>
2019-07-06 11:52:38 +0800

    子数组最大累加和//负数丢弃 正数保留。

--- src/BlueCup/_407_SubarrayMaxSum.java
+++ src/BlueCup/_407_SubarrayMaxSum.java
@@ -1,19 +1,18 @@
 package BlueCup;
 
 public class _407_SubarrayMaxSum {
-    //返回 子数组最大累加和
+    //返回 子数组最大累加和//负数丢弃 正数保留。
     public static void main(String[] args) {
         int[] arr = {1, -2, 3, 5, -2, 6, -1};
         int ans = check(arr);
         System.out.println(ans);
     }
-
-    private static int check(int[] arr) {
+     static int check(int[] arr) {
         //负数丢弃 正数保留。
         int ans = 0;
         int sum = arr[0], max = sum;
         int left = 0, right = 0;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {//从第二个数字开始 第一个数字给max了
             if (sum >= 0) {
                 sum += arr[i];//和大于0
             } else {//否则小雨 0  前面都抛弃不要了
