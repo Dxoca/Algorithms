95cfaa622d013a253fc1f83a469740675b01a68d
e <987284242@qq.com>
2019-07-15 12:30:36 +0800

    败北

---
+++ src/CometOJ/Contest7_A.java
@@ -0,0 +1,44 @@
+package CometOJ;
+
+import java.util.Scanner;
+
+public class Contest7_A {
+    public static void main(String[] args) {
+        Scanner cin =new Scanner(System.in);
+        long t=cin.nextLong();
+        for (long i = 0; i < t; i++) {
+            long l=cin.nextLong();
+            long r=cin.nextLong();
+            solve(l,r);
+        }
+    }
+
+    /**
+     * 最小公倍数 最大  最小
+     * 最大公约数 最大 最小
+     * @param l
+     * @param r
+     */
+    private static void solve(long l, long r) {
+//最小公倍数 =量整数/最大公约数 最小
+
+        //最大公约数 最大 最小：
+        long ans2= gcd(l,r);//最小 公约数
+        long ans1=l>r?l:r;//最大
+
+
+        long anss1=l*r/ans2;//初一最大的
+        long anss2=l*r/ans1;
+        System.out.print(anss1+" ");//公倍数 最大
+        System.out.print(anss2+" ");//最小
+        System.out.print(ans1+" ");//公约数 最大
+        System.out.println(ans2);//公约数 最小
+
+    }
+
+
+    private static long gcd(long   a, long b) {
+        return b==0?a:gcd(b,a%b);
+    }
+
+}
---
+++ src/CometOJ/Contest7_B.java
@@ -0,0 +1,48 @@
+package CometOJ;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Contest7_B {
+    static int count=0;
+    static StringBuffer S=new StringBuffer("");
+    public static void main(String[] args) {
+        Scanner cin =new Scanner(System.in);
+        char[] s=cin.nextLine().replace(" ","").toCharArray();
+        String ans="12341234";
+        while(ans.lastIndexOf(Sapp(s))==-1){
+            for (int i = 0,j=-1,k=-1; i < s.length; i++) {
+                if(ans.charAt(i)!=s[i]){
+                    if(j==-1)
+                        j=i;
+                    if(k==-1)
+                        k=i;
+                }
+                    swap(s,j,k);
+
+                }
+            }
+//        }
+        System.out.println(count);
+        /*
+            1234
+            1324
+            4321
+         */
+    }
+
+    private static String Sapp(char[] s) {
+        for (int i = 0; i < s.length; i++) {
+            S.append(s[i]);
+        }
+        return S.toString();
+    }
+
+    private static void swap(char[] s, int i, int j) {
+        char tamp=s[i];
+        s[i]=s[j];
+        s[j]=tamp;
+        count++;
+
+    }
+}
