9fe28a997eeb8abd381d931e23f5274f8312c113
e <987284242@qq.com>
2019-07-15 21:22:37 +0800

    水题 wa两次

---
+++ src/NowCoder/PAT_Basic_1001.java
@@ -0,0 +1,18 @@
+package NowCoder;
+import java.util.Scanner;
+
+
+public class PAT_Basic_1001 {
+    public static void main(String[] args){
+        Scanner cin =new Scanner(System.in);
+        int n=cin.nextInt();
+        for(long i=0;i<n;i++){//long 不用int,
+            long a=cin.nextLong(),b=cin.nextLong(),c=cin.nextLong();
+            boolean ans=false;
+            if(a>c-b){//思维转换 让数据变小 保险 不会超过最大value
+                ans=true;
+            }
+            System.out.println("Case #"+(i+1)+": "+ans);
+        }
+    }
+}
