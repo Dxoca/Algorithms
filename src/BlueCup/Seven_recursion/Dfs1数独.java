8b9864445cfe2897e05b2fc05d00c2fd28a012dd
e <987284242@qq.com>
2019-08-04 12:37:53 +0800

    数独游戏 dfs 深度优先搜索

---
+++ src/BlueCup/Seven_recursion/Dfs1数独.java
@@ -0,0 +1,54 @@
+package BlueCup.Seven_recursion;
+
+import java.util.Scanner;
+
+public class Dfs1数独 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        char[][] table = new char[9][];
+        for (int i = 0; i < 9; i++) {
+            table[i] = cin.nextLine().toCharArray();
+        }
+        dfs(table, 0, 0);
+    }
+    private static void print(char[][] table) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println(new String(table[i]));
+        }
+    }
+    public static void dfs(char[][] t, int x, int y) {
+        if (x==9){
+            print(t);
+            System.exit(0);//完全结束递归
+        }
+        if (t[x][y] == '0') {
+            for (int i = 1; i <= 9; i++) {
+                boolean res = check(t, x, y, i);
+                if (res) {
+                    t[x][y] = (char) ('0' + i);
+                    dfs(t, x + (y + 1) / 9, (y + 1) % 9);//状态转移
+                }
+            }
+            t[x][y] = '0';//回溯
+        } else {
+            dfs(t, x + (y + 1) / 9, (y + 1) % 9);//状态转移
+        }
+    }
+
+    private static boolean check(char[][] t, int x, int y, int i) {
+        for (int j = 0; j < 9; j++) {
+            int tmp = i + '0';//没有和i重复
+            if (t[x][j] == tmp || t[j][y] == tmp) {//行列
+                return false;
+            }
+            for (int k = (x/3)*3; k <(x/3+1)*3 ; k++) {//小矩形
+                for (int l = (y/3)*3; l <(y/3+1)*3 ; l++) {
+                    if(t[k][l]==tmp)return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+}
