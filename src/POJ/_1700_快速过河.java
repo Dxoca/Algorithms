56a9a783238adf06382bf70d2b9c9e7d84371410
e <987284242@qq.com>
2019-08-11 00:32:08 +0800

    up

--- src/POJ/_1700_快速过河.java
+++ src/POJ/_1700_快速过河.java
@@ -7,15 +7,16 @@ import static java.lang.Math.min;
 
 public class _1700_快速过河 {
     public static void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
-        int t = cin.nextInt();
-        for (int i = 0; i < t; i++) {
-            int n = cin.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
             int[] speed = new int[n];
             for (int j = 0; j < n; j++) {
-                speed[i] = cin.nextInt();
+                speed[j] = sc.nextInt();
             }
-            Arrays.sort(speed);//排序 左边最快 右边最大
+            //排序
+            Arrays.sort(speed);
             f(n, speed);
         }
     }
@@ -26,17 +27,20 @@ public class _1700_快速过河 {
         while (left > 0) {
             if (left == 1) {
                 ans += speed[0];
+                break;
             } else if (left == 2) {
-                ans += speed[0] + speed[1];
+                ans +=  speed[1];//两人就是 最慢的人
+                break;
             } else if (left == 3) {
                 ans += speed[0] + speed[1] + speed[2];
+                break;
             } else {
                 //第一种 最快的带最慢的 1，2-1； 1，3-1 ；1，4
                 int s1 = 2 * speed[0] + speed[left - 1] + speed[left - 2];
                 //第二种 顾眼前 相对最快都过去 回来一个 最快的
                 //12 出发 返回1 最后两个出发 返回2
                 //1 ，2-1；3，4——2 ； 1 2
-                int s2 = speed[1] + speed[01] + speed[left - 1] + speed[1];
+                int s2 = speed[1] + speed[0] + speed[left - 1] + speed[1];
                 ans += min(s1, s2);
                 left -= 2;//left代表剩余人数
             }
