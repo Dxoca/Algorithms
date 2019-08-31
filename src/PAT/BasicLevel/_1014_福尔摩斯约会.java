ae59146a92ec579e114d09f53f0ba80f210a6d59
e <987284242@qq.com>
2019-08-31 20:45:26 +0800

    20分

--- src/PAT/BasicLevel/_1014_福尔摩斯约会.java
+++ src/PAT/BasicLevel/_1014_福尔摩斯约会.java
@@ -14,43 +14,45 @@ public class _1014_福尔摩斯约会 {
         b1 = cin.next();
         b2 = cin.next();
         int week = 0;
-        String[] weeks={"MON","TUE","WED","THU","FRI","SAT","SUM"};
+        String[] weeks = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
         String hour = "";
         String minut = "";
         int aLen = min(a1.length(), a2.length());
         int count = 0;
         for (int i = 0; i < aLen; i++) {
             if (count == 0) {
-                if (a1.charAt(i) >= 'A' && a1.charAt(i) == a2.charAt(i)) {//大写
-                    week = a1.charAt(i) - 'A' ;//0~6 数组索引
+                if (a1.charAt(i) >= 'A' && a1.charAt(i) <= 'G'&& a1.charAt(i) == a2.charAt(i)) {//第 1 对相同的大写英文字母（大小写有区分）
+                    week = a1.charAt(i) - 'A';//0~6 数组索引
                     count++;
                     continue;//很关键
                 }
             }
             if (count == 1) {//相同的字符 钟头
-                if (a1.charAt(i) == a2.charAt(i)) {
-                    if (a1.charAt(i) >= '0' && a1.charAt(i) < '9') {
+                if (a1.charAt(i) == a2.charAt(i)) {//第 2 对相同的字符 于是一天的 0 点到 23 点由数字 0 到 9、以及大写字母 A 到 N 表示
+                    if (a1.charAt(i) >= '0' && a1.charAt(i) <= '9') {
                         hour = "0" + a1.charAt(i);
-                        count++;
+                        break;
                     } else if (a1.charAt(i) >= 'A' && a1.charAt(i) <= 'N') {
                         hour = String.valueOf(a1.charAt(i) - 'A' + 10);
-                        count++;
+                        break;
                     }
                 }
             }
         }
-
         //b starting
         int bLen = min(b1.length(), b2.length());
         for (int i = 0; i < bLen; i++) {
-            if ((b1.charAt(i) >= 'a' && b1.charAt(i) <= 'z' || b1.charAt(i) >= 'A' && b1.charAt(i) <= 'Z') && b1.charAt(i) == b2.charAt(i)) {
+            if ((b1.charAt(i) >= 'a' && b1.charAt(i) <= 'z' || b1.charAt(i) >= 'A' && b1.charAt(i) <= 'Z') && b1.charAt(i) == b2.charAt(i)) {//注意大写
                 if (i <= 9) {
                     minut = "0" + i;
-                } else
+                    break;
+                } else {
                     minut = "" + i;
+                    break;
+                }
             }
         }
-        System.out.print(weeks[week]+" "+hour+":"+minut);
+        System.out.print(weeks[week] + " " + hour + ":" + minut);
     }
 
 }
