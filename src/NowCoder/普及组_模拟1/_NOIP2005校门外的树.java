947eb450b02c5f2109dc787add1a2ecfc126569b
e <987284242@qq.com>
2020-08-03 10:42:33 +0800

    up

--- src/NowCoder/普及组_模拟1/_NOIP2005校门外的树.java
+++ src/NowCoder/普及组_模拟1/_NOIP2005校门外的树.java
@@ -1,19 +1,15 @@
 package NowCoder.普及组_模拟1;
 
 import java.util.Arrays;
-import java.util.Comparator;
+
 import java.util.Scanner;
 
 import static java.lang.Math.max;
 
-public class _NOIP2005校门外的叔 {
-    static class MyPair implements Comparable<MyPair> {
-        private Integer x;
-        private Integer y;
-
-        public Integer getX() {
-            return x;
-        }
+public class _NOIP2005校门外的树 {
+    static class MyPair<K, V> implements Comparable<MyPair> {
+        Integer x;
+        Integer y;
 
         public void setX(Integer x) {
             this.x = x;
@@ -23,12 +19,7 @@ public class _NOIP2005校门外的叔 {
             this.y = y;
         }
 
-        public Integer getY() {
-            return y;
-        }
-
         MyPair() {
-
         }
 
         MyPair(Integer x, Integer y) {
@@ -36,9 +27,17 @@ public class _NOIP2005校门外的叔 {
             setY(y);
         }
 
+        /**
+         * 调用Comparable接口 实现compareTo方法 便于直接sort
+         * 自定义类sort规则
+         *
+         * @param o
+         * @return
+         */
         @Override
         public int compareTo(MyPair o) {
-            return Integer.compare(this.x, o.x);
+            return this.x - o.x == 0 ? this.y - o.y : this.x - o.x;
+
         }
     }
 
@@ -47,13 +46,26 @@ public class _NOIP2005校门外的叔 {
         Scanner cin = new Scanner(System.in);
         int l = cin.nextInt();
         int n = cin.nextInt();
-        for (int i = 0; i < n; i++) seg[i] = new MyPair(cin.nextInt(), cin.nextInt());
-        Arrays.sort(seg, new Comparator<MyPair>() {
-            @Override
-            public int compare(MyPair o1, MyPair o2) {
-                return Integer.compare(o1.x, o2.x);
-            }
+        int a, b;
+        for (int i = 0; i < n; i++) {
+            a = cin.nextInt();
+            b = cin.nextInt();
+            seg[i] = new MyPair(a, b);
+        }
+//        Arrays.sort(seg,0,n);//使用类的比较器
+        Arrays.sort(seg, 0, n, (o1, o2) -> {//自建比较器 lambda
+            return o1.x - o2.x == 0 ? o1.y - o2.y : o1.x - o2.x;
         });
+        //比较繁杂的比较
+//        Arrays.sort(seg, 0, n, (o1, o2) -> {
+//            if (o1.x.compareTo(o2.x) == 0) {
+//                if (o1.y == o2.y) return 0;
+//                else if (o1.y > o2.y) return 1;
+//                else return -1;
+//            } else if (o1.x.compareTo(o2.x) > 0) return 1;
+//            else if (o1.x.compareTo(o2.x) < 0) return -1;
+//            else return 0;
+//        });
         int start = seg[0].x, end = seg[0].y;
         int sum = 0;
         for (int i = 1; i < n; i++) {//判断交集
@@ -68,20 +80,6 @@ public class _NOIP2005校门外的叔 {
         }
         sum += end - start + 1;//最后一个区间
         System.out.println(l + 1 - sum);//总长度减去被砍掉的
-//        //暴力O(2^6)
-//        int[] tree = new int[(int) 1e4 + 1];
-//        Scanner cin = new Scanner(System.in);
-//        int l = cin.nextInt();
-//        int t = cin.nextInt();
-//        for (int i = 0; i < t; i++) {
-//            int start = cin.nextInt();
-//            int end = cin.nextInt();
-//            while (start != end + 1)
-//                tree[start++] = -1;
-//        }
-//        int count = 0;
-//        for (int i = 0; i <= l; i++)
-//            if (tree[i] == 0) count++;
-//        System.out.println(count);
+
     }
 }
